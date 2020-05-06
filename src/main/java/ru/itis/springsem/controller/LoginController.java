c6978a5271f83b8a6e19c0a6d79e7be84469c6bd
Dmitry <loldragonmovies@gmail.com>
2020-05-06 23:58:38 +0300

    added RequestScope on LoginController

--- src/main/java/ru/itis/springsem/controller/LoginController.java
+++ src/main/java/ru/itis/springsem/controller/LoginController.java
@@ -4,10 +4,12 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.context.annotation.RequestScope;
 
 @Controller
 public class LoginController {
 
+    @RequestScope
     @GetMapping("/login")
     public String getLoginPage(Authentication authentication) {
         if (authentication != null) {
